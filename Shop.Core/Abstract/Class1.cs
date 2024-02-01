f04a870851f977606dfd213ab832ba6d35876a45
unknown <orkhansab@code.edu.az>
2024-02-01 16:04:39 +0400

    Added BaseEntities abstract class

---
+++ Shop.Core/Abstract/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace Shop.Core.Abstract;
+
+public abstract class BaseEntities
+{
+    public DateTime CreatedDate { get; set; }
+    public DateTime LastModifiedDate { get; set;}
+    public bool isActive { get; set; } = true;
+}
