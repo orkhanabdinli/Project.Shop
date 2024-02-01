422cbc541ed314b7562696595074e98bf5abaa4f
unknown <orkhansab@code.edu.az>
2024-02-01 17:28:43 +0400

    Added Brand class

---
+++ Shop.Core/Entities/Brand.cs
@@ -0,0 +1,9 @@
+﻿using Shop.Core.Abstract;
+
+namespace Shop.Core.Entities;
+
+public class Brand : BaseEntities
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
