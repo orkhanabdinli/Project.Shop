c54bc1a33f94658eb57b0fc5529f60c099cb911c
unknown <orkhansab@code.edu.az>
2024-02-01 17:15:26 +0400

    Added Cart class

---
+++ Shop.Core/Entities/Cart.cs
@@ -0,0 +1,9 @@
+﻿using Shop.Core.Abstract;
+
+namespace Shop.Core.Entities;
+
+public class Cart : BaseEntities
+{
+    public int Id { get; set; }
+    public User User { get; set; } = null!;
+}
--- Shop.Core/Entities/User.cs
+++ Shop.Core/Entities/User.cs
@@ -11,5 +11,6 @@ public class User : BaseEntities
     public string PhoneNumber { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
-    public Collection<Wallet> Wallets { get; set; }
+    public Collection<Wallet>? Wallets { get; set; }
+    public Cart Cart { get; set; } = null!;
 }
