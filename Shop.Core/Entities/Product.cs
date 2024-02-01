0d64629bdd123649ae980fca9cb7aa3ea3e6ec40
unknown <orkhansab@code.edu.az>
2024-02-01 17:24:27 +0400

    Added Product class

---
+++ Shop.Core/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using Shop.Core.Abstract;
+
+namespace Shop.Core.Entities;
+
+public class Product : BaseEntities
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public decimal? Price { get; set; } = null!;
+}
