c96ffb75c7bc3d63653c167139464121a49fd58d
unknown <orkhansab@code.edu.az>
2024-02-01 16:10:29 +0400

    Added User Class

---
+++ Shop.Core/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using Shop.Core.Abstract;
+
+namespace Shop.Core.Entities;
+
+public class User : BaseEntities
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Lastname { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
